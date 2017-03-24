66658ff5ca454f5216a0aa2d54cd8b66bb03d05a
clarkzsd <zsd1996@hotmail.com>
2017-03-24 17:04:24 +0800

    🎯Update package.json && Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## Future
+
+Oh, father tell me, do we get what we deserve.
+
+### Usage
+
+1. `git clone https://github.com/BetaSummer/Future` 
+2. `cd Future`
+3. `npm intsall`
+4. `npm run dev`
+
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var pkg = require('./package.json');
 var winston = require('winston');
 var expressWinston = require('express-winston');
 
+var env = process.env.NODE_ENV || 'development';
+var port = env === 'development' ? 3000 : 4667;
+
 app.set('views', path.join(__dirname, 'views'));// 设置存放模板文件的目录
 app.set('view engine', 'ejs');// 设置模板引擎为 ejs
 
@@ -32,6 +35,6 @@ app.use(function (err,req,res,next) {
   res.status(500).send('Something wrong!');
 });
 
-app.listen(3000, function () {
-  console.log(`${pkg.name} listening on port 3000`);
+app.listen(port, function () {
+  console.log(`${pkg.name} listening on port ${port}`);
 });
