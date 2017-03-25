099312c6ba3cf55fb72bf1181c3b5cc9c21195b6
clarkzsd <zsd1996@hotmail.com>
2017-03-25 12:08:18 +0800

    style changes

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.use(function (err,req,res,next) {
   res.status(500).send('Something wrong!');
 });
 
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
 app.listen(port, function () {
   console.log(`${pkg.name} listening on port ${port}`);
 });
